8414729a2ee8c833f02496008c088b8e8dd31c26
arthur <arthur.passuello@epfl.ch>
2017-10-08 19:38:50 +0200

    Fixed normal mode + comment #CaCompileraJamais

--- aff_min_max_17/src/normal.vhd
+++ aff_min_max_17/src/normal.vhd
@@ -35,57 +35,74 @@ architecture a_normal of normal is
 	signal temp_str			: std_logic_vector(15 downto 0);
 	signal temp_wek			: std_logic_vector(15 downto 0);
     signal osc_mask 		: std_logic_vector(15 downto 0);
-    signal str_mask 		: std_logic_vector(15 downto 0);
-    signal wk_mask  		: std_logic_vector(15 downto 0);
-    signal led_s    		: std_logic_vector(15 downto 0) := (others => '0');
+    signal str_mask 		: std_logic_vector(15 downto 0) := x"0000";
+    signal wk_mask  		: std_logic_vector(15 downto 0) := x"0000";
+    signal led_s    		: std_logic_vector(15 downto 0);
+    signal o_o_b			: std_logic_vector(15 downto 0);
     variable strong_range 	: integer range 0 to 15;
 	variable weak_range		: integer range 0 to 15;
 
-    component bin_lin use entity work.bin_lin is
-		port(
-			bin_i : in std_logic_vector(3 downto 0);
-        	lin_o : out std_logic_vector(15 downto 0)
-		);
-	end component;
+--    component bin_lin use entity work.bin_lin is
+--		port(
+--			bin_i : in std_logic_vector(3 downto 0);
+--        	lin_o : out std_logic_vector(15 downto 0)
+--		);
+--	end component;
 
 begin
 
-    osc :process (osc_i) is
-    begin
+    osc :process (osc_i) is	-- First the osc mask is computed in order to
+    begin					-- get either all '1' or all '0' 
         
         osc_mask <= (others => '1') when osc_i = '1' else (others => '0');
 
     end process;
 
-    strong :process (val_i, min_i) is
+    strong :process (val_i, min_i) is	-- Then strong mask is computed :
     begin
-		strong_range := to_integer(unsigned(val_i)) - to_integer(unsigned(min_i));
-        temp_str(0) <= '1';
-        if(strong_range < 15) then
-        	str_mask 	<= std_logic_vector(unsigned(temp_str) srl (strong_range+1));
-        	str_mask(0) <= '0';
-        	temp_str	<= str_mask;
-        	str_mask	<= std_logic_vector(unsigned(temp_str) srl (to_integer(unsigned(min_i)) - 1));
+    	-- First get the number of leds to be turned on : p. ex : val_i = 12, min_i = 8 
+    	-- range is 4 so 5 leds need to be turned on (8-9-10-11-12)
+		strong_range := to_integer(unsigned(val_i)) - to_integer(unsigned(min_i);
+        temp_str(0) <= '1';	-- LSB value will be duplicated when shift happens
+        if(min_i > 0) then	-- If min_i > 0, some leds will be switched off at the beginning
+        	-- Value is shifted one more bit so we can shift '0' afterwards
+        	str_mask 	<= std_logic_vector(unsigned(temp_str) srl (strong_range+1)); -- Strong range + 1 p.ex : 5 srl so 6 '1's
+        	str_mask(0) <= '0'; -- additionnal '1' replaced by a '0'
+        	temp_str	<= str_mask; 
+        	str_mask	<= std_logic_vector(unsigned(temp_str) srl (to_integer(unsigned(min_i)) - 1)); -- safe because min_i >= 1
         else
         	str_mask 	<= std_logic_vector(unsigned(temp_str) srl strong_range);
+        	-- resulting mask for the example above : 0000 0000 0000 0001 --> 0000 0000 0011 1111 --> 0000 0000 0011 1110
+        												--> 0000 0000 0111 1100
 		end if;
     end process;
 
-	weak :process (val_i, max_i) is
+	weak :process (val_i, max_i) is -- then mask for the led to be turned on with oscillating signal
     begin
 		weak_range := to_integer(unsigned(max_i)) - to_integer(unsigned(val_i));
         temp_wek(0) <= '1';
-        if(weak_range < 14) then
-        	wk_mask 	<= std_logic_vector(unsigned(temp_wek) srl (weak_range+1));
-        	wk_mask(0) 	<= '0';
-        	temp_wek	<= wk_mask;
-        	wk_mask		<= std_logic_vector(unsigned(temp_wek) srl (to_integer(unsigned(val_i) - 1));
-        else
-        	wk_mask 	<= std_logic_vector(unsigned(temp_wek) srl weak_range);
-        end if;
+		-- same idea as above except we are sure the LSLed won't ne an oscillating signal (max_i >= 1 p. def)
+       	wk_mask 	<= std_logic_vector(unsigned(temp_wek) srl (weak_range+1));
+        wk_mask(0) 	<= '0';
+       	temp_wek	<= wk_mask;
+       	if val_i > 0 then -- only applies if val_i > 0 as the LSB is already set to '0' just above
+       		wk_mask		<= std_logic_vector(unsigned(temp_wek) srl (to_integer(unsigned(val_i) - 1));
+       	end if;
+    end process;
+    
+    out_of_bound :process(val_i, max_i, val_i) is
+    begin
+    	-- This mask is relevant only if val_i is out of bounds, 
+    	-- it will set the output vector signal to '0' if that's the case
+    	if val_i < min_i OR val_i > max_i then
+    		o_o_b := x"0000";
+    	else
+    		o_o_b := x"FFFF";
+    	end if;
+    
     end process;
 
-	led_s <= str_mask OR (wk_mask AND osc_mask);
+	led_s <= (str_mask OR (wk_mask AND osc_mask) AND o_o_b);
 	
 	led_o <= led_s;
 end a_normal;
