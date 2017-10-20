9bd244fed0097fab9c22621f1a1271168e3c74f2
Lucas Elisei <lucas.elisei@heig-vd.ch>
2017-10-20 11:33:56 +0200

    Cleaned up min_max_top

--- aff_min_max_17/src/min_max_top.vhd
+++ aff_min_max_17/src/min_max_top.vhd
@@ -4,19 +4,19 @@
 --
 -- Fichier      : min_max_top.vhd
 --
--- Description  : 
--- 
+-- Description  :
+--
 -- Auteur       : Etienne Messerli
 -- Date         : 29.09.2014
--- Version      : 0.0
--- 
+-- Version      : 1.1
+--
 -- Utilise      : Manipulation Min_Max pour cours CSN
--- 
+--
 --| Modifications |------------------------------------------------------------
 -- Version   Auteur Date               Description
--- 1.0       EMI    29.09.2014    Version textuelle & mise à jour avec nouvelle 
---                                convention coding style   
---
+-- 1.0       EMI    29.09.2014    Version textuelle & mise à jour avec nouvelle
+--                                convention coding style
+-- 1.1 Passuello & Elisei 10.10.2017 Mapping pour le laboratoire 01 de CSN.
 -------------------------------------------------------------------------------
 
 library ieee;
@@ -24,7 +24,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 -- Ne pas modifier cette entity afin de repondre au test bench ainsi que la
--- simulation 
+-- simulation
 entity min_max_top is
     port(
         com_i  : in  std_logic_vector(1 downto 0);
@@ -33,7 +33,7 @@ entity min_max_top is
         osc_i  : in  std_logic;
         val_i  : in  std_logic_vector(3 downto 0);
         leds_o : out std_logic_vector(15 downto 0)
-        );
+    );
 end min_max_top;
 
 architecture struct of min_max_top is
@@ -42,35 +42,34 @@ architecture struct of min_max_top is
     -- Internal signal declarations --
     ----------------------------------
     signal Lin_o : std_logic_vector(leds_o'range);
-	signal Normal_o : std_logic_vector(leds_o'range);
+    signal Normal_o : std_logic_vector(leds_o'range);
 
     ----------------------------
     -- Component Declarations --
     ----------------------------
 
-	component normal is 
-		port(
-			min_i, max_i, val_i : in std_logic_vector(3 downto 0);
-  			osc_i : in std_logic;
-        	led_o : out std_logic_vector(15 downto 0)
-		);
-	end component;
+    component normal is
+        port(
+            min_i, max_i, val_i : in std_logic_vector(3 downto 0);
+            osc_i               : in std_logic;
+            led_o               : out std_logic_vector(15 downto 0)
+        );
+    end component;
 
-	component bin_lin is
-		port(
-			bin_i : in std_logic_vector(3 downto 0);
-        	lin_o : out std_logic_vector(15 downto 0)
-		);
-	end component;
+    component bin_lin is
+        port(
+            bin_i : in std_logic_vector(3 downto 0);
+            lin_o : out std_logic_vector(15 downto 0)
+        );
+    end component;
 
-	component choix is
-		port(
-			cmd: in std_logic_vector(1 downto 0);
-			led_00, led_01 : in std_logic_vector(15 downto 0);
-			led_o : out std_logic_vector(15 downto 0)
-		);
-	end component;
-
+    component choix is
+        port(
+            cmd            : in std_logic_vector(1 downto 0);
+            led_00, led_01 : in std_logic_vector(15 downto 0);
+            led_o          : out std_logic_vector(15 downto 0)
+        );
+    end component;
 
     --------------------------------------
     -- Optional embedded configurations --
@@ -82,27 +81,27 @@ begin
     ----------------------------
     -- Instance port mappings --
     ----------------------------
-	NML : normal
-		port map (
-			min_i => min_i,
-			max_i => max_i,
-			val_i => val_i,
-			osc_i => osc_i,
-			led_o => Normal_o
-		);
+    NML : normal
+        port map (
+            min_i => min_i,
+            max_i => max_i,
+            val_i => val_i,
+            osc_i => osc_i,
+            led_o => Normal_o
+        );
 
-	BL : bin_lin
-		port map (
-			bin_i => val_i,
-			lin_o => Lin_o
-		);
+    BL : bin_lin
+        port map (
+            bin_i => val_i,
+            lin_o => Lin_o
+        );
 
-	CHX : choix
-		port map (
-			cmd => com_i,
-			led_00 => Normal_o,
-			led_01 => Lin_o,
-			led_o => leds_o
-		);
+    CHX : choix
+        port map (
+            cmd => com_i,
+            led_00 => Normal_o,
+            led_01 => Lin_o,
+            led_o => leds_o
+        );
 
 end struct;
